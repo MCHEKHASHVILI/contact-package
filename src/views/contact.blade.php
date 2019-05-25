3ef6451fb2b3d8631c1869d684e65c9b082672e5
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2019-05-25 15:02:02 +0400

    modified view

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Us</title>
-</head>
-<body>
+@extends("layouts.app")
+@section('content')
     <h1>Contact us any time</h1>
     <form class="form" action="{{route('contact')}}" method="post">
      @csrf
@@ -15,5 +8,4 @@
         <textarea class="" name="message" cols="30" rows="10" placeholder="Your query"></textarea>
         <input class="" type="submit" value="Submit" />
      </form>
-</body>
-</html>
+@endsection
